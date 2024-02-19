16afe1b86f6b5a04ddb49eeaeb156fa8784ada18
Aidar <ar.ibraev98@gmail.com>
2024-02-19 20:20:23 +0600

    Add methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,26 @@ class EventsController < ApplicationController
   end
 
   def new
-    @event = Event.new
+    @event = current_user.created_events.build
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to @event, notice: 'Event was created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :body, :date)
   end
 end
