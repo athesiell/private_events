dddb4ccf2640f2ab34c3cc934e644d27b2689170
Aidar <ar.ibraev98@gmail.com>
2024-02-20 20:28:48 +0600

    Add params method to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,22 @@
 class BookingsController < ApplicationController
   def create
-    @event = Event.find(params[:event_id])
-    current_user.attended_events << @event
-    redirect_to @event, notice: 'You are attending this event'
+    @booking = current_user.bookings.build(booking_params)
+    if @booking.save
+      redirect_to @booking.attended_event, notice: 'You are now attending this event.'
+    else
+      render 'events/show'
+    end
   end
 
   def destroy
     @event = Event.find(params[:event_id])
     current_user.attended_events.delete(@event)
     redirect_to @event, notice: 'You will not attend this event'
+  end
+
+  private 
+
+  def booking_params
+    params.require(:booking).permit(:attended_event_id)
   end
 end
